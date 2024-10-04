69039e8290012db424aa8d8a0d6fd0361188bc04
Stephen <hellostephenwm@gmail.com>
2024-10-04 13:57:16 -0400

    6-1: expanded component to use the react query hook to fetch notes from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const App = () => {
-  const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+	const addNote = async (event) => {
+		event.preventDefault();
+		const content = event.target.note.value;
+		event.target.note.value = "";
+		console.log(content);
+	};
 
-  const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
-  }
+	const toggleImportance = (note) => {
+		console.log("toggle importance of", note.id);
+	};
 
-  const notes = []
+	const result = useQuery({
+		queryKey: ["notes"],
+		queryFn: () =>
+			axios.get("http://localhost:3001/notes").then((res) => res.data),
+	});
 
-  return(
-    <div>
-      <h2>Notes app</h2>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      {notes.map(note =>
-        <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
-        </li>
-      )}
-    </div>
-  )
-}
+	console.log(JSON.parse(JSON.stringify(result)));
 
-export default App
+	if (result.isLoading) {
+		return <div>loading data...</div>;
+	}
+
+	const notes = result.data;
+
+	return (
+		<div>
+			<h2>Notes app</h2>
+			<form onSubmit={addNote}>
+				<input name="note" />
+				<button type="submit">add</button>
+			</form>
+			{notes.map((note) => (
+				<li
+					key={note.id}
+					onClick={() => toggleImportance(note)}
+				>
+					{note.content}
+					<strong> {note.important ? "important" : ""}</strong>
+				</li>
+			))}
+		</div>
+	);
+};
+
+export default App;
