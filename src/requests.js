3328a953ea73c8776872472de2b23d14f6671cb0
Stephen <hellostephenwm@gmail.com>
2024-10-04 14:15:57 -0400

    6-2: add update note method to update selected note importance value

--- src/requests.js
+++ src/requests.js
@@ -31,3 +31,17 @@ export const createNote = (newNote) => {
 			console.error("error creating new note:", err.message);
 		});
 };
+
+// Method to update note importance
+export const updateNote = (updatedNote) => {
+	return axios
+		.put(`${baseUrl}/${updatedNote.id}`, updatedNote) // URL of the API/notToUpdate.id and the updated note object
+		.then((response) => {
+			// Return the data when request is successful
+			return response.data; // Extract the 'data' from the response object
+		})
+		.catch((err) => {
+			// Log any errors that occur during the request
+			console.error(`error updating note: ${updatedNote.id}`, err.message);
+		});
+};
