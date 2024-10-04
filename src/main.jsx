a8ce43b2644832e9225f279910c46d5a8ffce97e
Stephen <hellostephenwm@gmail.com>
2024-10-04 13:54:53 -0400

    6-1: initialized query client, wrapped App component in query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
-)
+import App from "./App.jsx";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<QueryClientProvider client={queryClient}>
+		<App />,
+	</QueryClientProvider>
+);
